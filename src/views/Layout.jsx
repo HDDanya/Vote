180cfe58519e3ebc7da7a0e76d5694c9b305b2fa
Anastasia Melnikova <nastasia.savina@gmail.com>
2022-11-08 15:09:10 +0300

    db:create

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,15 +1,15 @@
 const React = require('react');
 
 module.exports = function Layout({ children, newUser }) {
-    return (
-      <html lang="en">
+  return (
+    <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/css/normalize.css" />
         <link rel="stylesheet" href="/css/publicStyles.css" />
-        <script defer src='/js/publicScript.js'></script>
+        <script defer src="/js/publicScript.js" />
         <title>Privet</title>
       </head>
       <body>
@@ -17,5 +17,5 @@ module.exports = function Layout({ children, newUser }) {
         {children}
       </body>
     </html>
-  )
+  );
 };
