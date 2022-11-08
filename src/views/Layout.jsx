cf13181ad8b68452642a115a4a85356c942bc610
HdDanya <daniil24kiselev@gmail.com>
2022-11-08 14:55:10 +0300

    bootstrap

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,19 +1,53 @@
 const React = require('react');
 
-module.exports = function Layout({ children, newUser }) {
-    return (
-      <html lang="en">
+module.exports = function Layout({ children }) {
+  return (
+    <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
         <link rel="stylesheet" href="/css/normalize.css" />
         <link rel="stylesheet" href="/css/publicStyles.css" />
         <script defer src='/js/publicScript.js'></script>
         <title>Privet</title>
       </head>
       <body>
-        <h1>Привет из Layout</h1>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Вход</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Регистрация</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Создать Инициативу</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
         {children}
       </body>
     </html>
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,10 +1,17 @@
 const React = require('react');
 const Layout = require('./Layout')
 
-module.exports = function Main({ children, newUser }) {
-    return (
+module.exports = function Main({ children }) {
+  return (
     <Layout>
-      <h1>Привет из вьюшки Main</h1>
+      <div class="card" style={{width: 18+'rem'}}>
+        <img src="" class="card-img-top" alt="" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </Layout>
   )
 };
