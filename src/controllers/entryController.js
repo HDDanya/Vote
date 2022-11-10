b7bd5f9d10e55a4c2b4a36eefe06759c8dcb8a2d
HdDanya <daniil24kiselev@gmail.com>
2022-11-10 11:26:42 +0300

    votes

--- src/controllers/entryController.js
+++ src/controllers/entryController.js
@@ -3,26 +3,35 @@ const Entry = require('../views/Entry');
 const { Initiative, Golos } = require('../../db/models');
 
 const renderEntry = async (req, res) => {
-  const { user } = req.session;
-  try {
-    const title = await Initiative.findByPk(req.params.id);
-    renderTemplate(Entry, { title, user }, res);
-  } catch (error) {
-    console.log(error);
-  }
+    const { user } = req.session;
+    try {
+        const title = await Initiative.findByPk(req.params.id);
+        const voteOne = await Golos.findOne({ where: { InitiativeId: req.params.id, UserId: user.id } })
+        const voteAllPro = await Golos.findAll({ where: { InitiativeId: req.params.id, vote_pro: 1 } })
+        const voteAllAgainst = await Golos.findAll({ where: { InitiativeId: req.params.id, vote_against: 1 } })
+        console.log("VotePRO========", voteAllPro.length);
+        console.log("VoteAGAINST========", voteAllAgainst.length);
+        const votesObj={ pro: voteAllPro.length, against:voteAllAgainst.length }
+        renderTemplate(Entry, { title, user, voteOne, votesObj}, res);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 const renderVote = async (req, res) => {
-  try {
-    const { UserId, InitId } = req.body;
-    if (req.body.vote === 'yes') {
-      const response = await Golos.create({ UserId, InitiativeId: InitId, vote_pro: 1 });
-    } else {
-      const response = await Golos.create({ UserId, InitiativeId: InitId, vote_against: 1 });
+    try {
+        const { UserId, InitId } = req.body;
+
+        if (req.body.vote === 'yes') {
+            const response = await Golos.create({ UserId, InitiativeId: InitId, vote_pro: 1 });
+            res.sendStatus(200)
+        } else {
+            const response = await Golos.create({ UserId, InitiativeId: InitId, vote_against: 1 });
+            res.sendStatus(200)
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 module.exports = { renderEntry, renderVote };
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -6,20 +6,22 @@ module.exports = function Main({ title, user }) {
     <Layout isAuth={user}>
 
       <div className="card_container">
-        {title.map((el) => (
-          <div className="card" style={{ width: 18 + 'rem' }}>
-            {/* <img src="" className="card-img-top" alt="" /> */}
-            <div className="card-body">
-              <h5 className="card-title">
-                {el.title}
-              </h5>
-              <p className="card-text">
-                {el.body}
-              </p>
-              <a href={`/entry/${el.id}`} className="btn btn-primary">Подробнее</a>
+        <>
+          {title.map((el) => (
+            <div className="card" style={{ width: 18 + 'rem' }}>
+              {/* <img src="" className="card-img-top" alt="" /> */}
+              <div className="card-body">
+                <h5 className="card-title">
+                  {el.title}
+                </h5>
+                <p className="card-text">
+                  {el.body}
+                </p>
+                <a href={`/entry/${el.id}`} className="btn btn-primary">Подробнее</a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </>
       </div>
     </Layout>
   );
