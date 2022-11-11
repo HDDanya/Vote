642ed0ef5be89dac20a7da076f32e88e726e894a
HdDanya <daniil24kiselev@gmail.com>
2022-11-11 10:44:06 +0300

    appearance of user inits page

--- src/controllers/userInits.js
+++ src/controllers/userInits.js
@@ -1,14 +1,14 @@
 const renderTemplate = require('../lib/renderReactModule');
 const UserInits = require('../views/UserInits');
-const { Initiative } = require('../../db/models');
+const { Initiative, Golos } = require('../../db/models');
 
 const renderUserInits = async (req, res) => {
   console.log(req.params);
   console.log(req.body);
-  const { user } = req.session;
   try {
-    const allTitle = await Initiative.findAll({ where: { UserID: req.params.id } });
-    const title = await allTitle.map((el) => el.dataValues);
+    const allTitle = await Initiative.findAll({ include: Golos });
+    const title = await allTitle.map((el) => el.get({ plain: true }));
+    const { user } = req.session;
     renderTemplate(UserInits, { title, user }, res);
   } catch (error) {
     console.log(error);
--- src/views/UserInits.jsx
+++ src/views/UserInits.jsx
@@ -8,22 +8,42 @@ module.exports = function ({ title, user }) {
     <Layout isAuth={user}>
 
       <div className="card_container">
-        <>
-          {title.map((el) => (
-            <div className="card" style={{ width: 18 + 'rem' }}>
-              {/* <img src="" className="card-img-top" alt="" /> */}
-              <div className="card-body">
-                <h5 className="card-title">
-                  {el.title}
-                </h5>
-                <p className="card-text">
-                  {el.body}
-                </p>
-                <a href={`/entry/${el.id}`} className="btn btn-primary">Подробнее</a>
-              </div>
+        {title.map((el) => (
+          <div className="card" style={{ width: `${18}rem` }}>
+            {/* <img src="" className="card-img-top" alt="" /> */}
+            <div className="card-body">
+              <h5 className="card-title">
+                {el.title}
+              </h5>
+
+              <h2>
+
+                {(el.Golos.filter((g) => g.vote_pro === 1).length)
+                  / (el.Golos.filter((g) => g.vote_against === 1).length
+                    + el.Golos.filter((g) => g.vote_pro === 1).length) * 100 ? (`${(el.Golos.filter((g) => g.vote_against === 1).length
+                      + el.Golos.filter((g) => g.vote_pro === 1).length) * 100}%`) : ('Еще никто не голосовал')}
+
+              </h2>
+
+             
+              Общее количество голосов:
+              <h5 className="card-title">
+                {(el.Golos.filter((g) => g.vote_against === 1).length
+                  + el.Golos.filter((g) => g.vote_pro === 1).length)}
+              </h5>
+              Уровень:
+              <h5 className="card-title">
+                {el.level}
+              </h5>
+
+
+              {/* <p className="card-text">
+                {el.body}
+              </p> */}
+              <a href={`/entry/${el.id}`} className="btn btn-primary">Подробнее</a>
             </div>
-          ))}
-        </>
+          </div>
+        ))}
       </div>
     </Layout>
   );
