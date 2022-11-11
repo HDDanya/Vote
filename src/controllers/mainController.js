a6a879c6c444d0d308f26538ca1ee60bc485c992
Anastasia Melnikova <nastasia.savina@gmail.com>
2022-11-11 10:41:37 +0300

    add init seeders

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,10 +3,10 @@ const Main = require('../views/Main');
 const { Initiative, Golos } = require('../../db/models');
 
 const renderMain = async (req, res) => {
- try {
-  const allTitle = await Initiative.findAll({ include: Golos });
-  const title = await allTitle.map((el) => el.get({ plain: true }));
-  const { user } = req.session;
+  try {
+    const allTitle = await Initiative.findAll({ where: { status: 'active' }, include: Golos });
+    const title = await allTitle.map((el) => el.get({ plain: true }));
+    const { user } = req.session;
     renderTemplate(Main, {
       title, user,
     }, res);
